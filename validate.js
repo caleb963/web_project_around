f70dccf7a9c9b52431b307b43d275ac600c208de
Abai369 <jcaleb1110@gmail.com>
2024-03-31 19:51:53 -0700

    <feat>(functions and logic)

---
+++ validate.js
@@ -0,0 +1,52 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.{inputElememt.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+};
+
+checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement, settings) => {
+  const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+   console.log("valor inputs ->", inputList);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = (settings) => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  console.log("valor forms ->" , formList);
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, settings);
+  });
+};
+
+
+enableValidation({
+  formSelector: ".popup__edit-form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__input-submit",
+  inactiveButtonClass: "popup__button__disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visble"
+}); // ---> ejecuta enableValidation
