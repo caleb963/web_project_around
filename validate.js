2cd71867e5eebb681d54f1bd0db475cee3697656
Abai369 <jcaleb1110@gmail.com>
2024-04-01 20:14:04 -0700

    <feat>(logic of validation and some styles for type error)

--- validate.js
+++ validate.js
@@ -1,23 +1,24 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  console.log(`estos son los settings en showInputError ->`, settings)
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("form__input_type_error");
+  inputElement.classList.add(settings.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
+  errorElement.classList.add(settings.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, settings) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.classList.remove(settings.errorClass);
   errorElement.textContent = "";
 };
 
-checkInputValidity = (formElement, inputElement) => {
+checkInputValidity = (formElement, inputElement, settings) => {
   console.log("es valido el input?", inputElement.validity.valid)
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, settings);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, settings);
   }
 };
 
@@ -25,7 +26,7 @@ const setEventListeners = (formElement, settings) => {
   const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
      inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
+      checkInputValidity(formElement, inputElement, settings);
     });
   });
 };
@@ -47,5 +48,5 @@ enableValidation({
   submitButtonSelector: ".popup__input-submit",
   inactiveButtonClass: "popup__button__disabled",
   inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visble"
+  errorClass: "popup__error_visible"
 }); // ---> ejecuta enableValidation
