84e7ec14b6836a4aec370230bd3323ea2c13e85d
Abai369 <jcaleb1110@gmail.com>
2024-04-23 20:48:31 -0700

    <fix><errors on card.js>

--- Card.js
+++ Card.js
@@ -4,17 +4,16 @@ export default class Card {
     this.link = link;
     this.template = template;
 
-    this._handleLike = () => this._handleLike();
-    this._handleDislike =() => this._handleDislike();
-    this._handleRemoveCard = () => this._handleRemoveCard();
-    this._handleOpenImageCard = () => this._handleOpenImageCard();
+    this._handleLike = this._handleLike.bind(this);
+    this._handleRemoveCard = this._handleRemoveCard.bind(this);
+    this._handleOpenImageCard = this._handleOpenImageCard.bind(this);
   }
 
   _getCardClone(){
-   return  this.card = this.template.content.querySelector(".elements__card").cloneNode(true);
+   this.card = this.template.content.querySelector(".elements__card").cloneNode(true);
   }
 
-  _handleLike = () =>{
+  _handleLike = () => {
     const likeButton = this.card.querySelector(".elements__card-heart");
     likeButton.classList.toggle("elements__card-heart_active");
   }
@@ -36,7 +35,7 @@ export default class Card {
    setEventListeners(){
     this.card.querySelector(".elements__card-heart").addEventListener("click", this._handleLike);
     this.card.querySelector(".elements__card-delete").addEventListener("click", this._handleRemoveCard);
-    this.card.querySelector(".elements__card-image").addEventListener("click", this._handleOpenImageCard);
+    this.card.querySelector(".elements__card-image").addEventListener("click", () => this._handleOpenImageCard(this.title, this.link));
    }
 
    generateCard(){
