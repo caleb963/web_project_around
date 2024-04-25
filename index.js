a66c7d964b1e3517d06f76ea7014871804e2e140
Abai369 <jcaleb1110@gmail.com>
2024-04-24 21:34:31 -0700

    <feat><organize my code to send the project>

--- index.js
+++ index.js
@@ -2,31 +2,17 @@ import Card from "./Card.js";
 import FormValidator from "./FormValidator.js";
 import {handleOpenProfileForm, handleCloseProfileForm, handleOpenCardForm, handleCloseCardForm, handleCloseImage, handleProfileSubmit, handleAddCardSubmit} from "./utils.js";
 
-
-
-//const popUpProfile = document.querySelector("#popup-profile");
 const buttonProfile = document.querySelector("#profile-edit-button");
 const buttonCloseProfile = document.querySelector("#close-profile-form");
-//const profileName = document.querySelector(".profile__name");
-//const profileAbout = document.querySelector(".profile__about");
-//const inputProfileName = document.querySelector("#profile-input-name");
-//const inputProfileAbout = document.querySelector("#profile-input-about");
 const formProfile = document.querySelector("#profile-form");
 const formProfileSubmit = document.querySelector("#profile-submit");
-//const popUpAddCard = document.querySelector("#popup-add-card");
 const buttonAddCard = document.querySelector("#profile-add-button");
 const buttonCloseCard = document.querySelector("#close-addcard-form");
 const templateCard = document.querySelector(".template__card");
 const cardArea = document.querySelector(".elements");
-//const inputCardTitle = document.querySelector("#input-card-title");
-//const inputCardLink = document.querySelector("#input-card-url");
 const formCard = document.querySelector("#addcard-form");
 const buttonSubmitCard = document.querySelector("#addcard-submit");
-//const popUpShowPicture = document.querySelector("#popup-show-picture");
 const closeImage = document.querySelector("#close-popup-image");
-//const popUpTitle = document.querySelector(".popup__image-title");
-//const popUpImage = document.querySelector(".popup__image")
-//const overlay = document.querySelector("#overlay");
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -53,68 +39,6 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
   }
 ];
-
-
-
-// open and closed profile form
-
-/*function handleOpenProfileForm () {
-  popUpProfile.classList.add("popup__opener");
-   // change the popup display:block
-  overlay.style.display = "block";
-  document.addEventListener("click", handleCloseOutside);
-  document.addEventListener("keydown", handleCloseOnEscape);
-}*/
-
-/*function handleCloseProfileForm () {
-  popUpProfile.classList.remove("popup__opener");
-  // change the popup for display:none;
-  overlay.style.display = "none";
-  document.removeEventListener("click", handleCloseOutside);
-  document.removeEventListener("keydown", handleCloseOnEscape);
-}*/
-
-// open and closed card form
-
-/*function handleOpenCardForm () {
-  popUpAddCard.classList.add("popup__opener");
-  // change the popup display:block
-  overlay.style.display = "block";
-  console.log("open card form");
-  document.addEventListener("click", handleCloseOutside);
-  document.addEventListener("keydown", handleCloseOnEscape);
-} */
-
-/*function handleCloseCardForm () {
-  popUpAddCard.classList.remove("popup__opener");
-  // change the popup for display:none
-  overlay.style.display = "none";
-  document.removeEventListener("click", handleCloseOutside);
-  document.removeEventListener("keydown", handleCloseOnEscape);
-}*/
-
-// open and closed popup image
- /*function handleOpenImage(title, link){
-  popUpImage.src = link;
-  popUpTitle.textContent = title;
-  popUpShowPicture.classList.add("popup__opener");
-  document.addEventListener("click", handleCloseOutside);
-  document.addEventListener("keydown", handleCloseOnEscape);
-
-}*/
-
-/*function handleCloseImage() {
-  popUpShowPicture.classList.remove("popup__opener");
-  document.removeEventListener("click", handleCloseOutside);
-  document.removeEventListener("keydown", handleCloseOnEscape);
-}*/
-
-/*function handleProfileSubmit (evt) {
-  evt.preventDefault();
-  profileName.textContent = inputProfileName.value;
-  profileAbout.textContent = inputProfileAbout.value;
-  handleCloseProfileForm();
-}*/
 
 export function cardGenerator(title, link) {
  const card = templateCard.content.querySelector(".elements__card").cloneNode(true);
@@ -155,25 +79,6 @@ closeImage.addEventListener("click", handleCloseImage);
 formProfile.addEventListener("submit", handleProfileSubmit);
 formCard.addEventListener("submit", handleAddCardSubmit);
 
-// close the form when clicking outside
-
-/*function handleCloseOutside(evt) {
-  if(evt.target.classList.contains("popup__opener")){
-  handleCloseProfileForm();
-  handleCloseCardForm();
-  handleCloseImage();
-  }
-};*/
-
-// close the form with escape key
-
-/*function handleCloseOnEscape(evt) {
-  if(evt.key === "Escape"){
-    handleCloseProfileForm();
-    handleCloseCardForm();
-    handleCloseImage();
-  }
-};*/
 
 const profileFormValidation = new FormValidator(formProfile, {
   formSelector: ".popup__edit-form",
