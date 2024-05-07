933d2c86e685bd36e1a91b11b6eaf46ad5fbc19f
Abai369 <jcaleb1110@gmail.com>
2024-05-06 21:52:24 -0700

    <feat installation of webpack and two files json and one foldernode and starting to do the section class>

---
+++ script/Section.js
@@ -0,0 +1,13 @@
+export default class Section {
+  constructor({ items, renderer}, selector) {
+    this.items= items;
+    this._renderer = renderer;
+    this.container = document.querySelector(selector);
+  }
+  renderItems() {
+    this.items.forEach((item) => this._renderer(item));
+  }
+  addItem(element) {
+    this.container.prepend(element);
+  }
+}
