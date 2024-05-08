d3a540be3e9f9133a0bf92dfdf67b99a55fc3db4
Abai369 <jcaleb1110@gmail.com>
2024-05-07 20:16:02 -0700

    <feat><webpack config webpack html plugin><error on npm build and npm dev one for json and 4 for classes imports>

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+    clean: true,
+  },
+  target: ['web', 'es5'],
+  stats: { children: true },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
+}
