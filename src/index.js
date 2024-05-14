acce19473af9029e15e5bfdd8806ed647a8fadea
Abai369 <jcaleb1110@gmail.com>
2024-05-14 15:49:23 -0700

    <fIX><correction of handleOpenImage>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles/index.css";
 import Card from "../script/Card.js";
 import FormValidator from "../script/FormValidator.js";
 import Section from "../script/Section.js";
-import {handleOpenProfileForm, handleCloseProfileForm, handleOpenCardForm, handleCloseCardForm , handleCloseImage, handleProfileSubmit, handleAddCardSubmit} from "../script/utils.js";
+import {handleOpenProfileForm, handleCloseProfileForm, handleOpenCardForm, handleCloseCardForm , handleOpenImage, handleCloseImage, handleProfileSubmit, handleAddCardSubmit} from "../script/utils.js";
 
 
 const buttonProfile = document.querySelector("#profile-edit-button");
